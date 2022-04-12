bea10a35c680fe7441a4862faac834da33031897
rokku <70775740+rokkuu@users.noreply.github.com>
2022-04-12 20:44:19 +0200

    4/12/ - PlayerTeleport

--- src/main/java/com/github/rokkuu/bitofeverything/PlayerTeleport.java
+++ src/main/java/com/github/rokkuu/bitofeverything/PlayerTeleport.java
@@ -2,10 +2,14 @@ package com.github.rokkuu.bitofeverything;
 
 import jdk.tools.jlink.plugin.Plugin;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class PlayerTeleport implements CommandExecutor {
 
@@ -17,15 +21,24 @@ public class PlayerTeleport implements CommandExecutor {
         if (sender instanceof Player){
             Player player = (Player) sender;
             if (args.length > 0){
-                try {
+
+                if (Bukkit.getServer().getPlayerExact(args[0]) != null) {
                     Player destPlayer = sender.getServer().getPlayer(args[0]);
+                    assert destPlayer != null;
                     player.teleport(destPlayer);
 
-                } catch (Exception e){
-                    // e.printStackTrace();
+                    class DestinationPlayer {
+                        private HashMap<Player, Boolean> players;
+
+
+                    }
+
+                } else {
+                    player.sendMessage(ChatColor.RED + "Unknown player, try again.");
                 }
+
             } else {
-                player.sendMessage("Unknown player, please try again.");
+                player.sendMessage(ChatColor.RED + "Something went wrong, please try /tpa <nickname>");
             }
         }
         return false;
