930f425a591b3eadf616829195198677cdc6b4ed
rokku <70775740+rokkuu@users.noreply.github.com>
2022-04-12 18:12:57 +0200

    initial commit

--- src/main/java/com/github/rokkuu/bitofeverything/PlayerTeleport.java
+++ src/main/java/com/github/rokkuu/bitofeverything/PlayerTeleport.java
@@ -1,5 +1,6 @@
 package com.github.rokkuu.bitofeverything;
 
+import jdk.tools.jlink.plugin.Plugin;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,22 +9,23 @@ import org.bukkit.entity.Player;
 
 public class PlayerTeleport implements CommandExecutor {
 
+    private Bitofeverything plugin = Bitofeverything.getPlugin(Bitofeverything.class);
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if (sender instanceof Player){
             Player player = (Player) sender;
             if (args.length > 0){
-
                 try {
                     Player destPlayer = sender.getServer().getPlayer(args[0]);
                     player.teleport(destPlayer);
 
                 } catch (Exception e){
-                    e.printStackTrace();
+                    // e.printStackTrace();
                 }
             } else {
-                player.sendMessage("Please enter the nickname!");
+                player.sendMessage("Unknown player, please try again.");
             }
         }
         return false;
